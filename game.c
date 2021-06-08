312b004f6e6878eb9b3409173b3186b913456fd8
Spiro Stameson <sstameso@caltech.edu>
2021-06-08 09:31:27 -0700

    Add stubs

--- game.c
+++ game.c
@@ -3,7 +3,34 @@
 
 #include "vector.h"
 
+typedef struct {
+    /* TODO */
+} GameState;
+
+GameState *init(void) {
+    /* TODO */
+}
+
+void update(GameState *state) {
+    /* TODO */
+}
+
+void render(GameState *state) {
+    /* TODO */
+}
+
 int main(void)
 {
-    printf("hello, world!\n", y);
+    /*
+     * This is what the game does:
+     *
+     * - Initialize the state
+     * - Initialize a window
+     *
+     * while (running)
+     * {
+     *      - Update the state
+     *      - Render the game
+     * }
+     */
 }
