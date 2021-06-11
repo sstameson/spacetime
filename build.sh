cdbd38ed91382767baeef79bd966cbc1f39199df
Spiro Stameson <sstameso@caltech.edu>
2021-06-11 08:55:21 -0700

    Reformat build script

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 CC="clang"
-CFLAGS="-Wall -Werror -fsanitize=address -lSDL2 -lSDL2_gfx -lSDL2_mixer -Isrc/include"
-LIB="src/library/"
-CFILES="${LIB}sdl_wrapper.c ${LIB}polygon.c ${LIB}vector.c ${LIB}collision.c src/game.c"
+
+CFLAGS="-Wall -Werror -fsanitize=address "
+CFLAGS+="-lSDL2 -lSDL2_gfx -lSDL2_mixer -Isrc/include"
+
+BASE="src/library/"
+CFILES="${BASE}sdl_wrapper.c "
+CFILES+="${BASE}polygon.c "
+CFILES+="${BASE}vector.c "
+CFILES+="${BASE}collision.c "
+CFILES+="src/game.c"
 
 $CC $CFLAGS $CFILES -o game
