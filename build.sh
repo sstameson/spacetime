627d60dc2bdca79af292b9883ac8503ba5b1e9d1
Spiro Stameson <sstameso@caltech.edu>
2021-06-11 08:43:09 -0700

    Restructure project directory

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 CC="clang"
-CFLAGS="-Wall -Werror -fsanitize=address -lSDL2 -lSDL2_gfx -lSDL2_mixer"
+CFLAGS="-Wall -Werror -fsanitize=address -lSDL2 -lSDL2_gfx -lSDL2_mixer -Isrc/include"
+LIB="src/library/"
+CFILES="${LIB}sdl_wrapper.c ${LIB}polygon.c ${LIB}vector.c ${LIB}collision.c src/game.c"
 
-$CC $CFLAGS sdl_wrapper.c polygon.c vector.c collision.c game.c -o game
+$CC $CFLAGS $CFILES -o game
